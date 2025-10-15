5a1ccdfd821ac6bba50aeb12866b4d37d676d573
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-16 06:33:30 +0800

    chore: Update BepInDependency to version `0.9.0` and rename `PostAwake` to `Init`

--- Main.cs
+++ Main.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace BepinExUtils.Console;
 
 [BepInUtils("io.github.ykysnk.BepinExUtils.Console", "BepinEx Utils Console", Version)]
-[BepInDependency("io.github.ykysnk.BepinExUtils", "0.8.1")]
+[BepInDependency("io.github.ykysnk.BepinExUtils", "0.9.0")]
 [ConfigBind<KeyCode>("ConsoleToggleKey", SectionOptions, KeyCode.F2, "Toggle Console")]
 [ConfigBind<int>("ConsoleLogLimit", SectionOptions, 1000, "Max log count in Console")]
 [ConfigBind<string>("ConsoleLogFormat", SectionOptions, "[{0:HH:mm:ss}] [{1}] ({2}): {3}", "Log format")]
@@ -15,7 +15,7 @@ public partial class Main
     private const string SectionOptions = "Options";
     private const string Version = "0.0.1";
 
-    protected override void PostAwake()
+    public void Init()
     {
         var obj = new GameObject("BepinExUtils.Console", typeof(Behaviour.Console), typeof(CommandHandler));
         DontDestroyOnLoad(obj);
